0c29d17f18acf5d485ae65c77730fc986354be3f
Thadeu Antonio Ferreira de Melo <thadeu_afmelo@hotmail.com>
2019-04-04 18:34:48 -0300

    Add payment controller

--- src/main/java/com/invillia/acme/PaymentController.java
+++ src/main/java/com/invillia/acme/PaymentController.java
@@ -1,7 +1,42 @@
 package com.invillia.acme;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PaymentController {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Autowired OrderRepository orderRepository;
+
+    @GetMapping("/payments")
+    public List<Payment> retrievePayments(){ return paymentRepository.findAll();}
+
+    @PostMapping("/payment/{orderid}")
+    public ResponseEntity<Object> addPaymentToOrder(@RequestBody Payment payment, @PathVariable Integer orderid){
+        Optional<Order> orderOptional = orderRepository.findById(orderid);
+
+        if(!orderOptional.isPresent())
+            throw new RuntimeException("id - " + orderid);
+        Order order = orderOptional.get();
+
+        payment.setOrder(order);
+
+        Payment savedPayment = paymentRepository.save(payment);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedPayment.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+
+    }
 }
