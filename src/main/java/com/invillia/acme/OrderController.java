47dfb28e069e819ca6dff9b77fecf4ad70f38b27
Thadeu Antonio Ferreira de Melo <thadeu_afmelo@hotmail.com>
2019-04-04 22:42:23 -0300

    Move OrderOrderItem to new file.

--- src/main/java/com/invillia/acme/OrderController.java
+++ src/main/java/com/invillia/acme/OrderController.java
@@ -53,36 +53,7 @@ public class OrderController {
         return ResponseEntity.created(location).build();
     }
 
-    public class OrderOrderItem{
-        private Order order;
-        private List<OrderItem> orderItems;
-        public OrderOrderItem(){
-            orderItems = new ArrayList<>();
-            order = new Order();
-        }
 
-        public OrderOrderItem(Order order, List<OrderItem> orderItems) {
-            this.order = order;
-            this.orderItems = orderItems;
-        }
-
-        public Order getOrder() {
-            return order;
-        }
-
-        public void setOrder(Order order) {
-            this.order = order;
-        }
-
-
-        public List<OrderItem> getOrderItems() {
-            return orderItems;
-        }
-
-        public void setOrderItems(List<OrderItem> orderItems) {
-            this.orderItems = orderItems;
-        }
-    }
     @PostMapping("/orderwithitems")
     public ResponseEntity<Object> addOrderWithItems(@RequestBody OrderOrderItem orderOrderItem){
         Order order = orderOrderItem.getOrder();
