577848455aedc339781c643dc73c0aba7bd28534
Thadeu Antonio Ferreira de Melo <thadeu_afmelo@hotmail.com>
2019-04-05 00:58:54 -0300

    Add payment refund logic

--- src/main/java/com/invillia/acme/PaymentController.java
+++ src/main/java/com/invillia/acme/PaymentController.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class PaymentController {
@@ -38,5 +41,30 @@ public class PaymentController {
                 .toUri();
         return ResponseEntity.created(location).build();
 
+    }
+
+    @PostMapping("/payment/refund/{orderid}")
+    public ResponseEntity<Object> refundOrder(@PathVariable Integer orderid){
+        Optional<Order> orderOptional = orderRepository.findById(orderid);
+
+        if(!orderOptional.isPresent())
+            throw new RuntimeException("id - " + orderid);
+        Order order = orderOptional.get();
+
+        List<Payment> payments = order.getPayments();
+        Optional<Payment> p = payments.stream().max(Comparator.comparing(Payment::getPaymentDate));
+
+        if(!p.isPresent()){
+            throw  new RuntimeException("paymentdate error");
+        }
+        Date date = new Date();
+        Long diff = p.get().getPaymentDate().getTime() - date.getTime();
+        Integer daysDiff = Math.toIntExact(TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS));
+
+        if(daysDiff <= 10 && order.getStatus().equals("Concluded")){
+            return  ResponseEntity.ok("Refund success");
+        }
+
+        return ResponseEntity.ok("Could not refund");
     }
 }
