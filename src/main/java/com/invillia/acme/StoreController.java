3e102f7f72b5aaeb29b1462e3ad5553e19980b55
Thadeu Antonio Ferreira de Melo <thadeu_afmelo@hotmail.com>
2019-04-04 23:07:58 -0300

    Add update store

--- src/main/java/com/invillia/acme/StoreController.java
+++ src/main/java/com/invillia/acme/StoreController.java
@@ -1,6 +1,7 @@
 package com.invillia.acme;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -36,6 +37,26 @@ public class StoreController {
                 .buildAndExpand(savedStore.getId())
                 .toUri();
         return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping("/store/{id}")
+    public ResponseEntity<Object> updateStore(@RequestBody Store store, @PathVariable Integer id){
+        Optional<Store> storeOptional = storeRepository.findById(id);
+
+        if(!storeOptional.isPresent()){
+            return new ResponseEntity<>(new CustomErrorType("Unable to upate. User with id " + id + " not found."),
+                    HttpStatus.NOT_FOUND);
+        }
+        Store oldStore = storeOptional.get();
+        if(store.getName() != null && !store.getName().isEmpty()){
+            oldStore.setName(store.getName());
+        }
+        if(store.getAddress() != null && !store.getAddress().isEmpty()){
+            oldStore.setAddress(store.getAddress());
+        }
+
+        storeRepository.save(oldStore);
+        return null;
     }
 
 }
